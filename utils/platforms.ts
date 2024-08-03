664b8f7f3593767044a92b8dcd3e606b7773bee4
Kiril Zyapkov <ziapkov@gmail.com>
2024-08-03 22:30:49 +0300

    Constructing the Player class.

--- utils/platforms.ts
+++ utils/platforms.ts
@@ -1,3 +1,5 @@
 export class Platforms {
-  constructor() {}
+  constructor() {
+    
+  }
 }
--- utils/player.ts
+++ utils/player.ts
@@ -1,3 +1,32 @@
+const gravity: number = 0.5;
+
 export class Player {
-    constructor(){}
+  position: { x: number; y: number };
+  velocity: { x: number; y: number };
+  with: number;
+  height: number;
+  constructor() {
+    this.position = {
+      x: 10,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 5,
+    };
+    this.with = 30;
+    this.height = 30;
+  }
+
+  draw(c?: any) {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.with, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    this.velocity.y += gravity;
+  }
 }
