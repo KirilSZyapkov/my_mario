558d2629baf66b882f09f07be34c594baa856646
Kiril Zyapkov <ziapkov@gmail.com>
2024-08-04 21:25:17 +0300

    Customising the Platform class.

--- utils/platforms.ts
+++ utils/platforms.ts
@@ -1,5 +1,22 @@
 export class Platforms {
-  constructor() {
-    
+  position: {
+    x: number;
+    y: number;
+  };
+  with: number;
+  height: number;
+  
+  constructor({ x, y }: { x: number; y: number }) {
+    this.position = {
+      x,
+      y,
+    };
+    this.with = 500;
+    this.height = 30;
+  }
+
+  draw(c?: any) {
+    c.fillStyle = "blue";
+    c.fillRect = (this.position.x, this.position.y, this.with, this.height);
   }
 }
