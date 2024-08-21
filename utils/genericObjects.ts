c754914afe5f37f5390cfe33e59bf447df7707de
Kiril Zyapkov <ziapkov@gmail.com>
2024-08-21 12:51:55 +0300

    Creating new class for generic objects.

---
+++ utils/genericObjects.ts
@@ -0,0 +1,23 @@
+export class genericObjects {
+  position: {
+    x: number;
+    y: number;
+  };
+  width: number;
+  height: number;
+  image: any;
+
+  constructor(x: number, y: number, image: any) {
+    this.position = {
+      x,
+      y,
+    };
+    this.image = image;
+    this.width = this.image.width;
+    this.height = this.image.height;
+  }
+
+  draw(c: CanvasRenderingContext2D) {
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
